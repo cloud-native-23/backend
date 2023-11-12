b4b880e9676406006913658b7a61ad5bd5b54bcf
cozy <whatamath0626@gmail.com>
2023-11-12 17:23:44 +0800

    delete table StadiumCourtDisable and add StadiumDisable

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from .order import Order
 from .stadium_available_time import StadiumAvailableTime
-from .stadium_court_disable import StadiumCourtDisable
+from .stadium_disable import StadiumDisable
 from .stadium_court import StadiumCourt
 from .stadium import Stadium
 from .team_member import TeamMember
