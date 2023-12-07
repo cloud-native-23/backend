b90cc7fc94c16495b751735ad7ea83e58f5a5294
cozy <whatamath0626@gmail.com>
2023-12-07 21:43:37 +0800

    update send_email function

--- app/email/send_email.py
+++ app/email/send_email.py
@@ -1,32 +1,33 @@
 import smtplib
 from email.mime.text import MIMEText
 from jinja2 import Environment, FileSystemLoader
+from app.core.config import settings
+from fastapi import BackgroundTasks
 
+def get_formatted_html(result):
+    html_format = '''
+    <html>
+    <body style="margin: 0; padding: 0; box-sizing: border-box; font-family: Arial, Helvetica, sans-serif;">
+    <div style="width: 100%; background: #efefef; border-radius: 10px; padding: 10px;">
+    <div style="margin: 0 auto; width: 90%; text-align: center;">
+        <h1 style="background-color: rgba(0, 53, 102, 1); padding: 5px 10px; border-radius: 5px; color: white;">Stadium Notifications</h1>
+        <div style="margin: 30px auto; background: white; width: 40%; border-radius: 10px; padding: 50px; text-align: center;">
+        <h3 style="margin-bottom: 100px; font-size: 24px;">{}!</h3>
+        </div>
+    </div>
+    </div>
+    </body>
+    </html>
+    '''.format(result)
+    return html_format
 
-def send_email(result):
-    template_folder = "./templates"  # 模板文件所在的目录
-
-    # 使用绝对路径构建模板文件路径
-    template_path = os.path.join(template_folder, "email.html")
-    print('template_path',template_path)
-
-    # 创建模板加载器
-    env = Environment(loader=FileSystemLoader(template_folder))
-    
-    # 加载模板
-    template = env.get_template("email.html")
-
-    result_html = template.render(result)
-
-    GMAIL_USERNAME = "cloudnativeg23"
-    GMAIL_APP_PASSWORD = "kdsojnesnwrdyhob"
-    recipients = ["chien00772211@gmail.com"]
+async def send_email(title, mail_content, recipients: list):
+    result_html = get_formatted_html(mail_content)
     msg = MIMEText(result_html, 'html') 
-    msg["Subject"] = "Email report: a simple sum"
+    msg["Subject"] = title
     msg["To"] = ", ".join(recipients)
-    msg["From"] = f"{GMAIL_USERNAME}@gmail.com"
+    msg["From"] = settings.ADMIN_EMAIL
     smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp_server.login(GMAIL_USERNAME, GMAIL_APP_PASSWORD)
+    smtp_server.login(settings.ADMIN_NAME, settings.ADMIN_PASSWORD)
     smtp_server.sendmail(msg["From"], recipients, msg.as_string())
     smtp_server.quit()
-    
